b1913579e71d883cb5ed2b6e7ec4e6025e105937
CharuH <97988749+CharuH@users.noreply.github.com>
2025-06-04 14:32:36 -0400

    first message

---
+++ dietPlan/src/dietPlan/Main.java
@@ -0,0 +1,19 @@
+package dietPlan;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		frame.setSize(400, 400);
+		
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		
+		
+		frame.setVisible(true);
+		
+	}
+}
---
+++ dietPlan/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module dietPlan {
+	requires java.desktop;
+}
