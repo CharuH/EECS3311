ee92780d27b7b42f85f752bec662e6dacf7dba9a
xicao-cs <jason2005cao@gmail.com>
2025-06-25 13:43:47 -0400

    Update Dbfetch.java

--- dietPlan/src/dietPlan/Dbfetch.java
+++ dietPlan/src/dietPlan/Dbfetch.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Dbfetch {
-	private static final String URL = "jdbc:mysql://localhost:3306/helloworld";
-    private static final String USER = "java";
-    private static final String PASSWORD = "password";
+	private static final String URL = "jdbc:mysql://localhost:3306/3311_database";
+    private static final String USER = "root";
+    private static final String PASSWORD = "adminRomeo";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
