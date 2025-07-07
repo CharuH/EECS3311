29df79039cfa122b0af243fe3c829b50f4810024
xicao-cs <jason2005cao@gmail.com>
2025-07-07 19:44:43 -0400

    Update CFGDAO.java

--- EECS3311App/src/main/java/cfg/CFGDAO.java
+++ EECS3311App/src/main/java/cfg/CFGDAO.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 
 public class CFGDAO {
 	private static String url = "jdbc:mysql://localhost:3306/3311_database"; // Replace with correct DB
-    private static String user = "root"; // Replace with correct username
-    private static String pass = "adminRomeo"; // Replace with correct password
+    private static String user = "java"; // Replace with correct username
+    private static String pass = "password"; // Replace with correct password
 
     private Connection connection = null;
     
