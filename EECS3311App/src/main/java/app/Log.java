0e851a5ea2d8b5f8865637b51af53b3523d4dfaf
xicao-cs <jason2005cao@gmail.com>
2025-07-07 20:55:07 -0400

    Update Log.java

--- EECS3311App/src/main/java/app/Log.java
+++ EECS3311App/src/main/java/app/Log.java
@@ -6,13 +6,13 @@ import accCreate.MainUI;
 
 import java.util.List;
 
-public class Log extends JTabbedPane {
+public class Log extends JTabbedPane{
     public Log(MainUI main) {
         // Load food list from DB (once)
         List<Food> allFoods = MealFoodDAO.getAllFoods();
 
-        // Switch between MealBuilder and Viewer
-        add("Log Meal", new MealBuilderPanel(allFoods));
+        String username = main.getUser().getUsername();
+        add("Log Meal", new MealBuilderPanel(allFoods, username));
         add("View Meals", new MealViewerPanel());
     }
 }
