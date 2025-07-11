c6fe445c2ec98c10126dcf93ca564318088fe60b
xicao-cs <jason2005cao@gmail.com>
2025-07-10 22:20:41 -0400

    Update ErrorWindow.java

--- EECS3311App/src/main/java/accCreate/ErrorWindow.java
+++ EECS3311App/src/main/java/accCreate/ErrorWindow.java
@@ -1,9 +1,14 @@
 package accCreate;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class ErrorWindow {
@@ -19,13 +24,36 @@ public class ErrorWindow {
         errorFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         errorFrame.setSize(300, 150);
         errorFrame.getContentPane().setBackground(Color.WHITE);
+        errorFrame.setLayout(new BorderLayout());
 
         // Create a label for the error message
         JLabel errorLabel = new JLabel(errorMessage, SwingConstants.CENTER);
         errorLabel.setForeground(Color.RED); // Set the error message to red
 
         // Add the label to the error window
-        errorFrame.add(errorLabel);
+        errorFrame.add(errorLabel, BorderLayout.CENTER);
+        
+        JButton okButton = new JButton("OK");
+        okButton.setBackground(new Color(58, 162, 224));
+		okButton.setContentAreaFilled(false);
+		okButton.addMouseListener(new MouseAdapter() {
+            // When the mouse enters the button, change color
+            public void mouseEntered(MouseEvent e) {
+                okButton.setContentAreaFilled(true);
+            }
+            
+            // When the mouse exits the button, reset the color
+            public void mouseExited(MouseEvent e) {
+                okButton.setContentAreaFilled(false); 
+            }
+        });
+        okButton.addActionListener(ev -> errorFrame.dispose());
+        okButton.setSize(80, 25);
+        
+        JPanel button = new JPanel();
+        button.setBackground(Color.WHITE);
+        button.add(okButton);
+        errorFrame.add(button, BorderLayout.SOUTH);
 
         errorFrame.setVisible(true);
 	}
