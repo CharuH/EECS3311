d47abca5756174b91436b3e7ecd075fe82b62d20
xicao-cs <jason2005cao@gmail.com>
2025-07-12 21:14:14 -0400

    Update SortNutrients.java

--- EECS3311App/src/main/java/dailyNutrition/SortNutrients.java
+++ EECS3311App/src/main/java/dailyNutrition/SortNutrients.java
@@ -2,10 +2,7 @@ package dailyNutrition;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
 public class SortNutrients {
 
