ca10a8241da8898490ad4ec79cf69cd7a14cd293
xicao-cs <jason2005cao@gmail.com>
2025-07-12 20:57:33 -0400

    Add files via upload

--- EECS3311App/src/main/java/chart/ChartStrategy.java
+++ EECS3311App/src/main/java/chart/ChartStrategy.java
@@ -1,10 +1,15 @@
 package chart;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 import org.jfree.chart.JFreeChart;
 
+import dailyNutrition.Nutrient;
+
 public interface ChartStrategy {
-	public JFreeChart createChart(HashMap<String, Double> data);
+	public JFreeChart createChart(HashMap<String, Double> data, String title, String yAxis, String xAxis);
+	public JFreeChart createChart(ArrayList<Entry<Nutrient, Double>> data, String title, String yAxis, String xAxis);
 
 }
