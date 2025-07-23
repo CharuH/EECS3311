5c592502dbdfd8ff72202222b41f34eeb01e2cab
Darkbunny486 <92635337+Darkbunny486@users.noreply.github.com>
2025-07-22 22:27:46 -0400

    Update MainUI.java

--- EECS3311App/src/main/java/accCreate/MainUI.java
+++ EECS3311App/src/main/java/accCreate/MainUI.java
@@ -69,7 +69,11 @@ public class MainUI extends JFrame {
         	nutrition = new DailyNutrition(this);
         	currentPanel = nutrition;
         	add(nutrition, BorderLayout.EAST);
-        } 
+        }  else if (screenName.equals("mealSwapAcrossTime")) {
+            acrossTimeMealSubstitution = new AcrossTimeMealSubstitution(this);
+            currentPanel = acrossTimeMealSubstitution;
+            add(acrossTimeMealSubstitution, BorderLayout.EAST);
+        }
         revalidate();
         repaint();
     }
