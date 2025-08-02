dc24e92ffe76a38c3398f8ecb9a7ae7a375ace6b
xicao-cs <jason2005cao@gmail.com>
2025-08-02 16:11:29 -0400

    Update ProfileM.java

--- EECS3311App/src/main/java/accCreate/ProfileM.java
+++ EECS3311App/src/main/java/accCreate/ProfileM.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -102,10 +103,8 @@ public class ProfileM extends ProfileTemplate {
 					}
 				} catch (Exception ex0) {
 					error = true;
-					ErrorWindow errorDoB = new ErrorWindow("Invalid Password");
-					errorDoB.errorMessage();
+					JOptionPane.showMessageDialog(null, "Invalid Password", "Error", JOptionPane.ERROR_MESSAGE);
 				}
-				
 				try {
 					year = Integer.parseInt(yearField.getText());
 					month = Integer.parseInt(monthField.getText());
@@ -117,8 +116,7 @@ public class ProfileM extends ProfileTemplate {
 					}
 				} catch (Exception ex1) {
 					error = true;
-					ErrorWindow errorDoB = new ErrorWindow("Invalid Date of Birth");
-					errorDoB.errorMessage();
+					JOptionPane.showMessageDialog(null, "Invalid Date of Birth", "Error", JOptionPane.ERROR_MESSAGE);				}
 				}
 				try {
 					height = Double.parseDouble(heightField.getText());
@@ -127,8 +125,7 @@ public class ProfileM extends ProfileTemplate {
 					}
 				} catch (Exception ex2) {
 					error = true;
-					ErrorWindow errorHeight = new ErrorWindow("Invalid Height");
-					errorHeight.errorMessage();
+					JOptionPane.showMessageDialog(null, "Invalid Height", "Error", JOptionPane.ERROR_MESSAGE);
 				}
 				try {
 					weight = Double.parseDouble(weightField.getText());
@@ -137,8 +134,7 @@ public class ProfileM extends ProfileTemplate {
 					}
 				} catch (Exception ex3) {
 					error = true;
-					ErrorWindow errorWeight = new ErrorWindow("Invalid Weight");
-					errorWeight.errorMessage();
+					JOptionPane.showMessageDialog(null, "Invalid Weight", "Error", JOptionPane.ERROR_MESSAGE);
 				}
 				if (error == false) {
 					main.profile1(username, password, measurement);
